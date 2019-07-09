d867b4827391b2bec8ff3a8e539813394429c34d
gaohl <you@example.com>
2019-07-09 10:24:33 +0800

    创建user服务

---
+++ sunyi-cloud-user/src/main/java/com/sunyi/cloud/UserApplication.java
@@ -0,0 +1,17 @@
+package com.sunyi.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrixDashboard
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
